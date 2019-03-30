5da7214b2486f42a314492573259a850e255c6de
wEPac <35659294+wEPac@users.noreply.github.com>
2019-03-30 20:34:26 +0100

    Update Button.h

--- Button.h
+++ Button.h
@@ -1,33 +1,52 @@
-/*
-	Button - a small library for Arduino to handle button debouncing
-	
-	MIT licensed.
+/* -----------------------------------------------------------------
+ * Fork from :
+ * 
+ * Button
+ * https://github.com/madleech/Button
+ * Button - a small library for Arduino to handle button debouncing
+ * 
+ * MIT licensed.
+ * 
+ * Copyright (c) 2016 Michael D K Adams 
+ * 
+ * -----------------------------------------------------------------
+ * Modified by Eric Paquot, 03-2019
+ * -----------------------------------------------------------------
 */
 
 #ifndef Button_h
 #define Button_h
+
+
+
 #include "Arduino.h"
+
+
+#define PRESSED     LOW
+#define RELEASED    HIGH
 
 class Button
 {
-	public:
-		Button(uint8_t pin);
-		void begin();
-		bool read();
-		bool toggled();
-		bool pressed();
-		bool released();
-		bool has_changed();
-		
-		const static bool PRESSED = LOW;
-		const static bool RELEASED = HIGH;
-	
-	private:
-		uint8_t  _pin;
-		uint16_t _delay;
-		bool     _state;
-		bool     _has_changed;
-		uint32_t _ignore_until;
+  public:
+    Button(uint8_t pin);
+
+    uint8_t     _delay;
+
+    void        begin();
+    bool        read();
+    bool        toggled();
+    bool        pressed();
+    uint32_t    pressedFor();
+    bool        released();
+    uint32_t    releasedFor();
+
+  private:
+    uint8_t     _pin;
+    bool        _state;
+    bool        _changed;
+    uint32_t    _debounce_ms;
+
+    bool        changed();
 };
 
 #endif
